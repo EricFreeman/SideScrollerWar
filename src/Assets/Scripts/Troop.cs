dbe3e1279057b27fd2e7c9ff1841810c3c6da875
Eric Freeman <eric.freeman@extendhealth.com>
2015-01-01 19:12:02 -0700

    Created a bunch of temp images for the first troop.

---
+++ src/Assets/Scripts/Troop.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Troop : MonoBehaviour
+    {
+        public List<Sprite> IdleAnimations;
+        public List<Sprite> WalkAnimations;
+        public List<Sprite> AttackAnimations;
+        public List<Sprite> DeathAnimations;
+    }
+}
